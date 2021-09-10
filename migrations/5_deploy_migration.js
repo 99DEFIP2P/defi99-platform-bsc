4f49fb4063a8b46c8b7e6524f939c510044f309a
Philliphrini <90440038+Philliphrini@users.noreply.github.com>
2021-09-10 16:02:47 +0530

    Updated admin address

--- migrations/5_deploy_migration.js
+++ migrations/5_deploy_migration.js
@@ -1,25 +1,21 @@
 require("dotenv").config();
 const verify = require("../src/verify");
 
-const Timelock = artifacts.require("Timelock");
+const Defi99Plus = artifacts.require("Defi99Plus");
 
-// Admin address for Timelock
+// Address for transferring Defi99Plus Token
 const ADMIN_ADDRESS = "0x4089bA22DE13C07Ff418D8f76E90ff1940936CB4"
 
 module.exports = async (deployer, network) => {
 
-    /* Deploy Timelock Contract */
-    await deployer.deploy(
-        Timelock,
-        ADMIN_ADDRESS,
-        172800
-    );
+    /* Deploy Defi99Plus Contract */
+    /*await deployer.deploy(Defi99Plus, ADMIN_ADDRESS);
 
     if (network !== "development")
         await verify.bscscanVerify(
-            Timelock,
+            Defi99Plus,
             network,
             process.env.BSCSCANAPIKEY,
             1
-        );
+        ); */
 };
