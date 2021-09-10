a9b55ed0409040d14e4140abb9da49cdb5afff2b
Philliphrini <90440038+Philliphrini@users.noreply.github.com>
2021-09-10 16:13:00 +0530

    Update 22_deploy_migration.js

--- migrations/22_deploy_migration.js
+++ migrations/22_deploy_migration.js
@@ -7,13 +7,13 @@ const Defi99Core = artifacts.require("Defi99Core");
 const JumpRateModel = artifacts.require("JumpRateModel");
 
 // Admin address for deploying dDEFI99
-const ADMIN_ADDRESS = "0xD565C800C1611Bed28911D5A819f1E6A9E0d3d7f"
+const ADMIN_ADDRESS = "0x4089bA22DE13C07Ff418D8f76E90ff1940936CB4"
 
 module.exports = async (deployer, network) => {
     let DEFI99_TOKEN_ADDRESS = "0xd5cf8d4e332667ded826fdb758a2b3dda98d600b";
 
     /* Deploy Defi99 DEFI99 */
-    await deployer.deploy(
+    /* await deployer.deploy(
         DErc20DEFI99Delegator,
         DEFI99_TOKEN_ADDRESS,
         Defi99Core.address,
@@ -33,5 +33,5 @@ module.exports = async (deployer, network) => {
             network,
             process.env.BSCSCANAPIKEY,
             1
-        );
+        ); */
 };
