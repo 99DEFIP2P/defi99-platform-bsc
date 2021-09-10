cad05ce52050aa86b6d844a2cfe07c22804fa783
Philliphrini <90440038+Philliphrini@users.noreply.github.com>
2021-09-10 16:08:58 +0530

    Updated address

--- migrations/20_deploy_migration.js
+++ migrations/20_deploy_migration.js
@@ -7,10 +7,10 @@ const Defi99Core = artifacts.require("Defi99Core");
 const JumpRateModel = artifacts.require("JumpRateModel");
 
 // Admin address for deploying dLINK
-const ADMIN_ADDRESS = "0xD565C800C1611Bed28911D5A819f1E6A9E0d3d7f"
+const ADMIN_ADDRESS = "0x4089bA22DE13C07Ff418D8f76E90ff1940936CB4"
 
 module.exports = async (deployer, network) => {
-    let LINK_TOKEN_ADDRESS = "0x5AA034CCBDFc3CF8cbDe2711bf9C3fd90B96F24B";
+    let LINK_TOKEN_ADDRESS = "0xf8a0bf9cf54bb92f17374d9e9a321e6a111a51bd";
 
     /* Deploy 99defi LINK */
     await deployer.deploy(
