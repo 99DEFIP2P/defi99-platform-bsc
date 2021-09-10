80ec48fa7db87863f52f2dc5b061d196271a7fbc
Philliphrini <90440038+Philliphrini@users.noreply.github.com>
2021-09-10 16:08:02 +0530

    Update 17_deploy_migration.js

--- migrations/17_deploy_migration.js
+++ migrations/17_deploy_migration.js
@@ -6,7 +6,7 @@ const DErc20ZRXDelegate = artifacts.require("DErc20ZRXDelegate");
 module.exports = async (deployer, network) => {
 
     /* Deploy Defi99 DErc20ZRXDelegate */
-    await deployer.deploy(DErc20ZRXDelegate);
+    /* await deployer.deploy(DErc20ZRXDelegate);
 
     if (network !== "development")
         await verify.bscscanVerify(
@@ -14,5 +14,5 @@ module.exports = async (deployer, network) => {
             network,
             process.env.BSCSCANAPIKEY,
             1
-        );
+        ); */
 };
