b8ea5532aee65dc8050c0fcec20d14abc7410728
Philliphrini <90440038+Philliphrini@users.noreply.github.com>
2021-09-10 16:05:58 +0530

    Update 12_deploy_migration.js

--- migrations/12_deploy_migration.js
+++ migrations/12_deploy_migration.js
@@ -7,13 +7,13 @@ const Defi99Core = artifacts.require("Defi99Core");
 const JumpRateModel = artifacts.require("JumpRateModel");
 
 // Admin address for deploying dUSDT
-const ADMIN_ADDRESS = "0xD565C800C1611Bed28911D5A819f1E6A9E0d3d7f"
+const ADMIN_ADDRESS = "0x4089bA22DE13C07Ff418D8f76E90ff1940936CB4"
 
 module.exports = async (deployer, network) => {
     let USDT_TOKEN_ADDRESS = "0xe93e5891b1b757D51D20448F2803Db4Cd6f84233";
 
     /* Deploy 99defi USDT */
-    await deployer.deploy(
+    /*await deployer.deploy(
         DErc20Delegator,
         USDT_TOKEN_ADDRESS,
         Defi99Core.address,
@@ -33,5 +33,5 @@ module.exports = async (deployer, network) => {
             network,
             process.env.BSCSCANAPIKEY,
             1
-        );
+        );*/
 };
